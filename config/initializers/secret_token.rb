c4089d19ebecc4d60b15b76477f7f2db50e33da1
samosys <machine5@samosys015.(none)>
2015-09-23 18:23:20 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SuparMarket::Application.config.secret_key_base = '8bac92c2a1ff3e1815cfc0e7ccdef42b9bff46cbfa3bf3354a5ac5637370e8dd89defe50a7784c5d485d2b82a19fc4984023ee2c073095688b5ebad3c449f4b1'
